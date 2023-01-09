a8ac7b6634766e4d8468412bf23e14b866e88e1c
jMoirenc <julien.moirenc@oclock.io>
2023-01-09 17:14:08 +0100

    fin atelier j2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,29 @@
 
 // on objet qui contient des fonctions
 var app = {
+
+  base_url: 'http://localhost:3000',
+
+  async getListsFromAPI (){
+    try {
+      const response = await fetch(`${app.base_url}/lists`);
+      
+      if (!response.ok){
+        throw new Error('Pb pour récupérer les listes');
+      }
+
+      const lists = await response.json();
+
+      for (const list of lists){
+        app.makeListInDOM(list);
+      }
+
+    }catch (e){
+      console.log(e);
+    }  
+
+  },
+
   addListenerToActions(){
     const addListButtonElement = document.getElementById('addListButton');    
     addListButtonElement.addEventListener('click', app.showAddListModal);
@@ -12,12 +35,6 @@ var app = {
 
     const formAddListElement =  document.querySelector('#addListModal form');
     formAddListElement.addEventListener('submit', app.submitAddListForm);
-
-    const addCardButtonElements = document.querySelectorAll('.addCardButton');    
-
-    for (const addCardButtonElement of addCardButtonElements){
-      addCardButtonElement.addEventListener('click', app.showAddCardModal);
-    }    
 
     const formAddCardElement =  document.querySelector('#addCardModal form');
     formAddCardElement.addEventListener('submit', app.submitAddCardForm);
@@ -41,26 +58,40 @@ var app = {
     modalElement.querySelector('[name=list_id]').value = listId;
   },
 
-  hideModal(event){
-    const clickedButttonElement = event.currentTarget;
-    const modalElement = clickedButttonElement.closest('.modal');
-    modalElement.classList.remove('is-active');
+  hideModal(){
+    const modalElement = document.querySelector('.modal.is-active');
+    if (modalElement){
+      modalElement.classList.remove('is-active');
+    }    
   },
 
-  submitAddListForm(event){
+  async submitAddListForm(event){
     event.preventDefault();
     const formElement = event.currentTarget;
     const addListFormData = new FormData(formElement);
-    const data = Object.fromEntries(addListFormData); 
+    // const data = Object.fromEntries(addListFormData); 
 
-    // temporaire, en attendant l'api
-    data.id = Math.floor(Math.random() * 10000);
+    const response = await fetch(
+      `${app.base_url}/lists`,
+      {
+        method: 'POST',
+        body: addListFormData,
+      }
+    );
+      
+    if (!response.ok){
+      throw new Error('Pb pour ajouter la liste');
+    }
+
+    const data = await response.json();
+    
+    // data.id = Math.floor(Math.random() * 10000);
     
     app.makeListInDOM(data);
 
     formElement.reset();
 
-    app.hideModal(event);
+    app.hideModal();
   },
 
   makeListInDOM(data){
@@ -70,30 +101,49 @@ var app = {
     const newListElement = templateListElement.content.cloneNode(true);
     newListElement.querySelector('.okblist-title').textContent = data.name;
 
-    console.log(newListElement.dataset);
-
     newListElement.querySelector('.okblist').dataset.listId = data.id;
 
     newListElement.querySelector('.addCardButton').addEventListener('click', app.showAddCardModal);    
 
     const listsContainerElements = document.querySelector('.okblists');
     listsContainerElements.append(newListElement);
+
+    if (data.cards){
+      for (const card of data.cards){
+        app.makeCardInDOM(card);
+      }      
+    }
   },
 
-  submitAddCardForm(event){
+  async submitAddCardForm(event){
     event.preventDefault();
     const formElement = event.currentTarget;
     const addCardFormData = new FormData(formElement);
-    const data = Object.fromEntries(addCardFormData); 
+    
+    //const data = Object.fromEntries(addCardFormData); 
 
     // temporaire, en attendant l'api
-    data.id = Math.floor(Math.random() * 10000);
+    // data.id = Math.floor(Math.random() * 10000);
     
+    const response = await fetch(
+      `${app.base_url}/cards`,
+      {
+        method: 'POST',
+        body: addCardFormData,
+      }
+    );
+      
+    if (!response.ok){
+      throw new Error('Pb pour ajouter la carte');
+    }
+
+    const data = await response.json();
+
     app.makeCardInDOM(data);
 
     formElement.reset();
 
-    app.hideModal(event);
+    app.hideModal();
   },
 
   makeCardInDOM(data){
@@ -116,6 +166,7 @@ var app = {
   init() {
     console.log('app.init !!');
     app.addListenerToActions();
+    app.getListsFromAPI();
   }
 
 };
