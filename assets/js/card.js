3e46253875440fc52c690cb15a421b7357751a7d
jMoirenc <julien.moirenc@oclock.io>
2023-01-11 09:58:07 +0100

    suppression des cartes

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,37 +1,36 @@
 const cardModule = {
 
-  showAddCardModal(event){
+  showAddCardModal(event) {
 
     const clickedButton = event.currentTarget;
     const listElement = clickedButton.closest('.okblist');
     const listId = listElement.dataset.listId;
 
-    const modalElement = document.getElementById('addCardModal');    
+    const modalElement = document.getElementById('addCardModal');
 
     modalElement.classList.add('is-active');
 
     modalElement.querySelector('[name=list_id]').value = listId;
   },
 
-  async submitAddCardForm(event){
+  async submitAddCardForm(event) {
     event.preventDefault();
     const formElement = event.currentTarget;
     const addCardFormData = new FormData(formElement);
-    
+
     //const data = Object.fromEntries(addCardFormData); 
 
     // temporaire, en attendant l'api
     // data.id = Math.floor(Math.random() * 10000);
-    
+
     const response = await fetch(
-      `${utilsModule.base_url}/cards`,
-      {
+      `${utilsModule.base_url}/cards`, {
         method: 'POST',
         body: addCardFormData,
       }
     );
-      
-    if (!response.ok){
+
+    if (!response.ok) {
       throw new Error('Pb pour ajouter la carte');
     }
 
@@ -44,13 +43,34 @@ const cardModule = {
     utilsModule.hideModal();
   },
 
-  makeCardInDOM(data){
+  async deleteCard(event) {
+    const target = event.currentTarget;
+    const cardElement = target.closest('.okbcard');
+    const cardId = cardElement.dataset.cardId;
+    try {
+      if (confirm('on la supprime cette carte ?')) {
+        const response = await fetch(
+          `${utilsModule.base_url}/cards/${cardId}`, {
+            method: "DELETE",
+          },
+        );
+
+        cardElement.remove();
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  },
+
+  makeCardInDOM(data) {
     console.log(data);
     const templateCardElement = document.getElementById('cardTemplate');
     const newCardElement = templateCardElement.content.cloneNode(true);
     newCardElement.querySelector('.okbcard-content').textContent = data.content;
 
     newCardElement.querySelector('.okbcard').dataset.cardId = data.id;
+
+    newCardElement.querySelector('.okbcard').addEventListener('click', cardModule.deleteCard);
 
     const listElement = document.querySelector(`[data-list-id="${data.list_id}"]`);
 
